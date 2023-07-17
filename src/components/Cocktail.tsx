fe554acee97c09867a7781b7acae7b29eae2b1ec
JkrCode <40894524+JkrCode@users.noreply.github.com>
2023-07-17 12:08:17 +0200

    added lowercase ingredient flexibility

--- src/components/Cocktail.tsx
+++ src/components/Cocktail.tsx
@@ -1,31 +1,37 @@
 const Cocktail: React.FC<any> = function ({ data, ingredients }) {
-    const ingredientComponents = [];
-  
-    for (let i = 1; i <= 4; i++) {
-      const ingredientKey = `strIngredient${i}`;
-      const ingredientValue = data[ingredientKey];
-  
-      const Ingredient = () => {
+  const ingredientComponents = [];
+
+  for (let i = 1; i <= 4; i++) {
+    const ingredientKey = `strIngredient${i}`;
+    const ingredientValue = data[ingredientKey];
+
+    const Ingredient = () => {
+      if (ingredientValue !== null) {
+        const lowerCaseIngredient = ingredientValue.toLowerCase();
+
         if (
           ingredients !== undefined &&
-          ingredients.includes(ingredientValue)
+          ingredients.includes(lowerCaseIngredient)
         ) {
           return <p className="font-bold">{ingredientValue}</p>;
         } else {
           console.log(ingredients);
           return <p>{ingredientValue}</p>;
         }
-      };
-  
-      ingredientComponents.push(<Ingredient key={ingredientKey} />);
-    }
-  
-    return (
-      <div className="flex mb-4">
-        <p className="flex-none w-40 mr-4 h-16">{data.strDrink}</p>
-        <div className="flex-col">{ingredientComponents}</div>
-      </div>
-    );
-  };
-  
-  export default Cocktail;
+      } else {
+        return null; // or handle the null case as desired
+      }
+    };
+
+    ingredientComponents.push(<Ingredient key={ingredientKey} />);
+  }
+
+  return (
+    <div className="flex mb-4">
+      <p className="flex-none w-40 mr-4 h-16">{data.strDrink}</p>
+      <div className="flex-col">{ingredientComponents}</div>
+    </div>
+  );
+};
+
+export default Cocktail;
